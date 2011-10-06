5af326a25e66efe39a9c0b17c04995c7d2d7cfbc
Dan <ayotted@gmail.com>
2011-10-05 23:13:57 -0400

    Finished changing/commenting on everything to look/make more sense when read, next is logging

--- Project452.c
+++ Project452.c
@@ -9,23 +9,27 @@
 #include <sys/resource.h>
 #include <time.h>
 
-typedef int (gvpipe_t) [2];      // gvpipe_t is now a new user-defined type
-const int READ = 0;
-const int WRIT = 1;
-int i;
+typedef int (gvpipe_t) [2];
 
+const int READ = 0;
+const int WRITE = 1;
+
+//this is the method that the child runs
 void doWork(int id, gvpipe_t fd[], int N);
-int compare (const void * a, const void * b);
+
+//compares two integers. used for qsort
+int intCompare (const void * a, const void * b);
 
 
 int main (void) {
-    gvpipe_t *copper;
-    pid_t who;
-    int k;
-    int numFiles;
-    char text[254];     
-    
+    gvpipe_t *brass; //the pipes
+    pid_t pid; //the pids
+    int k; //a counter used ALOT throughout the code
+    int numFiles; //number of files to be sorted
+    char text[254]; //just used to receive input from the user
+	int i; //another counter used through out the main method
 
+	//receive how many files to be sorted
     fputs("How many files would you like to sort?\n", stdout);
 	fflush(stdout);
 	if (fgets(text, sizeof text, stdin) != NULL) {
@@ -34,126 +38,151 @@ int main (void) {
 		  *newLine = '\0';
     }
 
+	//convert the number of files to an integer
     numFiles = atoi (text);
   
-    copper = (gvpipe_t *) calloc (numFiles, sizeof(gvpipe_t));
+	//allocate enough memory so that there are enough pipes for the number of files
+    brass = (gvpipe_t *) calloc (numFiles, sizeof(gvpipe_t));
 
-    // create ALL the pipes BEFORE fork so all the children inherit them
+    //the pipes are now created for each child before the child is even created
     for (k = 0; k < numFiles; k++)
-        pipe (copper[k]);
+        pipe (brass[k]);
 
-    // create N children
+    //creating enough children to sort all of the files, 1 child per file
     for (k = 0; k < numFiles; k++) {
-        who = fork ();
-	int stat;
-        wait (&stat);
-        if (who == 0) {
-		doWork(k, copper, numFiles);
-		exit (0);           /* each child will end here */
-        }
-    }
-
-//       int arrayOfIntForMaster[numFiles][99];  
-
-      int z = 0;
-      int count = 0;
-      int val;
-      int size = 7; 
-	  //we assume the length of the files are always 7 so there are 
-	  //7 items inside a pipe given to the parent	
-      if (numFiles == 1) {
-  	do {
-	  read(copper[z][READ], &val, sizeof(int));
-	  //printf("Status: %d\n", status);
-	  printf("Num: %d\n", val);
-	  count++;
-	} while(count < size * (numFiles+1));
-      } else {
-	
-	  int values[numFiles];
-	  int pipes[numFiles];
-	  int counts[numFiles];
-	  int winner = -1; 
-	  
-	  //Everything starts out as 0!
-	  for (z = 0; z < numFiles; z++) {
-	      values[z] = 0;
-	      pipes[z] = 1;
-	      counts[z] = 0;
-	  }
-	/********************************************************************************************************/
-	int pipeNumber = 0;
-	  int t = 0;
-	  count = 0;
-	  while (count < size * numFiles) {
-	    
-	    count++;	    
-        /********************************************************************************************************/
-	      /*This should read in each time*/
-	      for (t = 0; t < numFiles; t++) {
-		if (pipes[t] == 1 && counts[t] < size) {
-		  read(copper[t][READ], &values[t], sizeof(int));
-		  //printf("Read from pipe %d: value %d\n", z, values[t]);
-		  pipes[t] = 0;
-		  counts[t]++;
-		}
-	      }//end of for loop
-      
-	      pipeNumber = 0;
-	      for (z = 0; z < numFiles-1; z++) {
-		printf("Determining winner...");
-		if (z == 0) {
-		  winner = values[z];
-		  pipeNumber = z;
-		 }
-		  if (winner > values[z+1]) {
-		    winner = values[z+1];
-		    pipeNumber = z+1;
-		  } else if (winner == values[z+1]) {
-		    if (counts[z] < counts[z+1]) {
-		      winner = values[z];
-		      pipeNumber = z;
-		    } else {
-		      winner = values[z+1];
-		      pipeNumber = z+1;
-		    }
-		  }
-		  
-		  if (counts[z] > size) {
-		      winner = z + 1;
-		      pipeNumber = z + 1;
-		  }
-		  if (counts[z+1] > size) {
-		      winner = z;
-		      pipeNumber = z;
-		  }
-		}//end of forloop
+        pid = fork ();
 		
-		printf("Winner: %d\n", winner); //This is the lowest number
-		printf("Pipe Number %d\n", pipeNumber);
-		printf("Counts of pipe number: %d\n", counts[pipeNumber]);
-		printf("Count is at : %d\n", count);
-		pipes[pipeNumber] = 1; //this sets the pipe to be read again
-	    }
-      }
-       /********************************************************************************************************/
-	   
-
-
-     /* now wait for all children to stop */
-    for (k = 0; k < numFiles; k++) {
-         int stat;
-         wait (&stat);
-     }
+		//I'm not 100% sure anymore on what this does but if I remember right 
+		//removing it makes things work to fast which causes it to crash
+		int stat;
+        wait (&stat);
+        
+		//each child will then doWork and the exit 
+		if (pid == 0) {
+			doWork(k, brass, numFiles);
+			exit (0);
+		}
+    }
+	
+    int z = 0;
+    int count = 0;
+    int val;
+	  
+    /**
+	 * We assume the length of the files are always 7 so there are 
+	 * 7 items inside a pipe given to the parent. The reason we 
+	 * assume this is because I had alot of difficulty with a
+	 * while loop that simply checked to make sure that read 
+	 * was still reading in numbers and not finished as it would 
+	 * wait for more info in the pipe even though the pipe was 
+	 * closed. Tryed to manually insert a large int to check for
+	 * at the end of a pipe but it resulted in the same out come.
+	 */
+    int size = 7; 
+	  
+	//if you only inputed one file do this!
+	if (numFiles == 1) {
+		do {
+			read(brass[z][READ], &val, sizeof(int));
+			printf("Num: %d\n", val);
+			count++;
+		} while(count < size * (numFiles+1));
+    } else {
+	
+		int val[numFiles]; //values to be stored (one at a time)
+		int pipes[numFiles]; //the pipes, which will then decide which to read
+		int timesTaken[numFiles]; //the number of times you've read from a pipe
+		int lowestNum = -1; //the result of the comparisons 
+		  
+		//Start al of them at 0 except for pipes as you want to read from 
+		//all the pipes the first time
+		for (z = 0; z < numFiles; z++) {
+			val[z] = 0;
+			pipes[z] = 1;
+			timesTaken[z] = 0;
+		}
+		 
+		int pipeNumber = 0; //this is the pipe to be changed to 1 to get the new one
+		int t = 0; //this is just a counter used later
+		count = 0; //this is also a counter used later but reset to 0
+	  
+		//so while you haven't read through all of the stuff in the pipes
+		while (count < size * numFiles) {
+		  
+			//increment count by 1, count isn't used except to count how many times the
+			//while loop runs
+			count++;	    
+			
+			for (t = 0; t < numFiles; t++) {
+				//each time a pipe is set to 1 in pipes, it will read from that pipe again
+				//provided that the pipe that you're reading in from is still less then
+				//the size of the input (which is predetermined)
+				if (pipes[t] == 1 && timesTaken[t] < size) {
+					read(brass[t][READ], &val[t], sizeof(int));
+					pipes[t] = 0;
+					timesTaken[t]++;
+				}
+			}//end of for loop
+		  
+			//This is the difficult part of the actual reading in.
+			//So all the info is stored in val[z] so you loop through
+			//numFiles - 1 so you can compare the current with the one
+			//coming up and set that as the lowestNumber, so you are
+			//constantly setting lowestNumber as you compare through 
+			//this.
+			for (z = 0; z < numFiles-1; z++) {
+				
+				//so you set the lowest number to be the first value
+				//in the first pipe to just start off.
+				if (z == 0) {
+					lowestNum = val[z]; //sets val[z] to the lowest number
+					pipeNumber = z; //sets the pipeNumber to z
+				}
+				
+				//next we see if the lowest number is greater then the 
+				//next value 
+				if (lowestNum > val[z+1]) {
+					lowestNum = val[z+1]; //sets the lowest number to val[z+1]
+					pipeNumber = z+1; //sets the pipe number to z+1
+				} else if (lowestNum == val[z+1]) { //if they are the same
+					//see which of them has been taken from more 
+					if (timesTaken[pipeNumber] < timesTaken[z+1]) {
+						lowestNum = val[z];
+						pipeNumber = z;
+					} else {
+						lowestNum = val[z+1];
+						pipeNumber = z+1;
+					}
+				}
+				
+				//So if the current winner has been taken from to many times 
+				//this method will search to see which file hasn't been taken
+				//from to many times and set that to the lowest value
+				//is NOT the BEST way or for that matter always correct way!
+				if (timesTaken[pipeNumber] > size) {
+					int test; //test variable
+					for (test = 0; test < numFiles; test++) {
+						if (timesTaken[test] < size) {
+							lowestNum = val[z + 1];
+							pipeNumber = z + 1;
+						}
+					}
+				}
+			}//end of for loop
+			
+			printf("%d\n", lowestNum); //This is the lowest number
+			pipes[pipeNumber] = 1; //this sets the pipe to be read again
+		}//end while
+    }//end long else statement
    
-    free (copper);
+	//brass's memory is now free and you then exit the program
+    free (brass);
     return 0;	
 }
 
 
-/**********************************************************
- * 		Child process
- **********************************************************/
+//This is the the method that child method runs before terminating
+//itself.
 void doWork(int id, gvpipe_t fd[], int N) {
 
 	char *fileName;
@@ -168,37 +197,37 @@ void doWork(int id, gvpipe_t fd[], int N) {
 	}
 
 	fileName = text;
-	int size = 80;
-	char line[80];
-	FILE *fr;
-	fr = fopen(fileName, "rt");
+	int size = 99;
+	char line[99];
+	FILE *file;
+	file = fopen(fileName, "rt");
 	int temp;
-	int arrayOfInt[80];
+	int intArray[99];
 	 i=0;
 	
-	while(fgets(line,80, fr) !=NULL){
-	  
+	while(fgets(line,99, file) !=NULL){
 	  sscanf(line, "%d", &temp);
-	  arrayOfInt[i] = temp;
+	  intArray[i] = temp;
 	  i++;
 	}
 	size=i;
 	
-	int x = 0;	
-	qsort (arrayOfInt, size, sizeof(int), compare);
+	int count = 0;	
+	qsort (intArray, size, sizeof(int), intCompare);
 	
-	while(x < i){
-	    printf("%d\n", arrayOfInt[x]);	
-	      write (fd[id][WRIT], &arrayOfInt[x], sizeof(int));
-	    x++;
+	//right all of your content to the pipe. 
+	while(count < i){
+	    write (fd[id][WRITE], &intArray[count], sizeof(int));
+	    count++;
 	}
 	
-	fclose(fr);
-	close(fd[id][WRIT]);
+	//here you're closing the file and closing the pipes
+	fclose(file);
+	close(fd[id][WRITE]);
  	close(fd[id][READ]);
 }
 
-int compare (const void * a, const void * b)
+int intCompare (const void * a, const void * b)
 {
   return ( *(int*)a - *(int*)b );
 }
